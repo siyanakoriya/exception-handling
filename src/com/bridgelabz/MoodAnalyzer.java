a99999f1cb0f059fabb587744f8d359bd21ed951
siyanakoriya <siyanakoriya122@gmail.com>
2022-09-29 08:08:52 +0530

    com.bridgelabz

---
+++ src/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,25 @@
+
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+
+    public String analyseMood(String message){
+        /*
+          null in message it should give happy but it shows nullPointer Exception
+         So we use try and catch for exception handling. if it is null then it gives sad using catch block
+         */
+        try {
+            message.contains(null);
+            System.out.println("HAPPY");
+            return "HAPPY MOOD";
+        }
+        catch (NullPointerException e){
+            System.out.println("SAD");
+            return "SAD MOOD";
+        }
+    }
+    public static void main(String[] args) {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+        moodAnalyser.analyseMood("I'm in any mood today");
+    }
+}
